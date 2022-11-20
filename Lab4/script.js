a2f04f7f52a85b1d3e4baf11aadfeecd412ef494
Christine <chuynh2@hawk.iit.edu>
2022-11-19 22:18:40 -0600

    Add search weather function

--- Lab4/script.js
+++ Lab4/script.js
@@ -4,7 +4,6 @@ let getCurrentLocation = () => {
       function fetchCurrentCoords(a, b){
         let latitude = a;
         let longitude = b;
-        loadingGif()
         alert("Weather for your location will be loaded.");
         fetch("https://weatherdbi.herokuapp.com/data/weather/"+ latitude + "," + longitude)
         .then((res) => {
@@ -20,7 +19,43 @@ let getCurrentLocation = () => {
          })
       };
       fetchCurrentCoords(position.coords.latitude, position.coords.longitude);
-    });
-};
+    })
+}
 
+//Get weather API went search button is clicked
+function searchWeather(){
+    let forecast = document.getElementById("forecast-grid");
+    let inputLocation = document.getElementById("search-input");
+    let todayForecast = document.getElementById("containter-today-forecast");
 
+    forecast.innerHTML="";
+
+    if (inputLocation==""){
+        alert("Please enter a valid location.");
+    } else{
+        let updatedInputLocation = inputLocation.replace(/\s/g,"");
+        fetch("https://weatherdbi.herokuapp.com/data/weather/" + updatedInputLocation)
+        .then((res) => {
+            if (res){
+                return res.json();
+            } else {
+                throw new Error ("Network Response Error");
+            }
+        })
+        .then (json => {
+            if(json.status){
+                alert(json+json.message);
+                todayForecast.innerHTML="";
+                forecast.innerHTML="";
+                todayForecast.innerHTML=("Error: " + json.status + " " + json.message + " enter valid input.")
+            } else {
+                displayWeather(json);
+                displayWeatherForecast;
+            }
+        })
+        .catch (error =>{
+            alert (error);
+            todayForecast.innerHTML = ("Error: " + error);
+        })
+    }
+}
